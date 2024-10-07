1742426531696126431cea5640a0f196655d021b
Usuario <Usuario@CPD03-M15>
2024-10-07 13:41:19 -0300

    Adicionar arquivos de projeto.

---
+++ AppGuiaCursos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppGuiaCursos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
